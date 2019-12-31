0349d6849cedc0b1a2d7a4a58f396f039786aa58
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2019-12-30 23:45:06 -0600

    new files

--- C/Basic/structure_as_parameter.c
+++ C/Basic/structure_as_parameter.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
+
 struct Rectangle{
   int length;
   int breath;
 };
 
+/*
+//Call by Value Method
 int area(struct Rectangle r1){
   r1.length++;
   return r1.length * r1.breath;
 }
+*/
+//Call by Address
+void changeLength(struct Rectangle *p, int l){
+  p->length = l;
+}
 
 int main(){
   struct Rectangle r = {10, 5};
-    printf("%d\n", area(r));
-    printf("%d\n%d\n", r.length, r.breath);
+/*
+  printf("%d\n", area(r));
+  //Call by value does not change the actual values
+  printf("%d\n%d\n", r.length, r.breath);
+*/
+  changeLength(&r, 20);
+  printf("%d\n",r.length);
+
 }
