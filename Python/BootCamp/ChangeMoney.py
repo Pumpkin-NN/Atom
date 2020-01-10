b7cef0cbf09d229ee6af99389470dcde16db0616
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2020-01-10 01:08:51 -0600

    correct outputs

--- Python/BootCamp/ChangeMoney.py
+++ Python/BootCamp/ChangeMoney.py
@@ -2,166 +2,108 @@
 # - The user enters a cost and then the amount of money given.
 #The program will figure out the change and the number of quarters, dimes, nickels, pennies needed for the change.
 
-def remainder_check(n):
+def remainder_check(r, n):
     count = 0
     while True:
-        r = r - n
+        r = round((r - n),2)
         count = count + 1
         if r < n:
             break
     return count
 
-def change(c, m):
+def count_change(r):
+    if r >= 0.25:
+        count = remainder_check(r, 0.25)
+        print(f'The quarter(s) is(are): {count}')
 
-    ce = round(m-c, 2)
-    print(f'The change is ${ce}')
-    if ce // 1 != 0:
-        print(f'The dollar: {round(ce // 1,1)}')
-        r = round((ce - (ce // 1)), 2)
-        if r != 0:
-            print(f'The remainder is :{r}')
-            if r >= 0.25:
-                count = 0
-                while True:
-                    r = r - 0.25
-                    count = count + 1
-                    if r < 0.25:
-                        break
-                print(f'The quarter(s) is(are): {count}')
-            elif r >= 0.10:
-                count = 0
-                while True:
-                    r = r - 0.10
-                    count = count + 1
-                    if r < 0.10:
-                        break
-                print(f'The dime(s) is(are): {count}')
-            elif r >= 0.05:
-                count = 0
-                while True:
-                    r = r - 0.05
-                    count = count + 1
-                    if r < 0.05:
-                        break
+        r = round((r - 0.25 * count),2)
+        if r >= 0.10:
+            count = remainder_check(r, 0.10)
+            print(f'The dime(s) is(are): {count}')
+
+            r = round((r - 0.10 * count),2)
+            if r >= 0.05:
+                count = remainder_check(r, 0.05)
                 print(f'The nickel(s) is(are): {count}')
-            else:
-                count = 0
-                while True:
-                    r = r - 0.01
-                    count = count + 1
-                    if r < 0.01:
-                        break
+
+                r = round((r - 0.05 * count),2)
+                if r >= 0.01:
+                    count = remainder_check(r, 0.01)
+                    print(f'The penny(s) is(are): {count}')
+            elif r >= 0.01:
+                count = remainder_check(r, 0.01)
                 print(f'The penny(s) is(are): {count}')
+
+        elif r >= 0.05:
+            count = remainder_check(r, 0.05)
+            print(f'The nickel(s) is(are): {count}')
+
+            r = round((r - 0.05 * count),2)
+            if r >= 0.01:
+                count = remainder_check(r, 0.01)
+                print(f'The penny(s) is(are): {count}')
+
+        else:
+            if r >= 0.01:
+                count = remainder_check(r, 0.01)
+                print(f'The penny(s) is(are): {count}')
+
+
+    elif r >= 0.10:
+        count = remainder_check(r, 0.10)
+        print(f'The dime(s) is(are): {count}')
+
+        r = round((r - 0.10 * count),2)
+        if r >= 0.05:
+            count = remainder_check(r, 0.05)
+            print(f'The nickel(s) is(are): {count}')
+
+            r = round((r - 0.05 * count),2)
+            if r >= 0.01:
+                count = remainder_check(r, 0.01)
+                print(f'The penny(s) is(are): {count}')
+        else:
+            if r >= 0.01:
+                count = remainder_check(r, 0.01)
+                print(f'The penny(s) is(are): {count}')
+
+
+
+    elif r >= 0.05:
+        count = remainder_check(r, 0.05)
+        print(f'The nickel(s) is(are): {count}')
+
+        r = round((r - 0.05 * count),2)
+        if r >= 0.01:
+            count = remainder_check(r, 0.01)
+            print(f'The penny(s) is(are): {count}')
 
 
     else:
-        print(f'The change dollar is :{0}')
+        if r >= 0.01:
+            count = remainder_check(r, 0.01)
+            print(f'The penny(s) is(are): {count}')
+
+
+
+def change(c, m):
+    ce = round(m-c, 2)
+    print(f'The total change is ${ce}')
+    if ce // 1 != 0:
+        print(f'The dollar(s) is(are): {int(ce // 1)}')
+        r = round((ce - (ce // 1)), 2)
         if r != 0:
-            print(f'The remainder is :{r}')
-            if r >= 0.25:
-                count = 0
-                while True:
-                    r = r - 0.25
-                    count = count + 1
-                    if r < 0.25:
-                        break
-                print(f'The quarter(s) is(are): {count}')
-
-                r = r - 0.25 * count
-                if r >= 0.10:
-                    count = 0
-                    while True:
-                        r = r - 0.10
-                        count = count + 1
-                        if r < 0.10:
-                            break
-                    print(f'The dime(s) is(are): {count}')
-
-                    r = r - 0.10 * count
-                    if r >= 0.05:
-                        count = 0
-                        while True:
-                            r = r - 0.05
-                            count = count + 1
-                            if r < 0.05:
-                                break
-                        print(f'The nickel(s) is(are): {count}')
-
-                        r = r - 0.05 * count
-                        if r >= 0.01:
-                            count = 0
-                            while True:
-                                r = r - 0.01
-                                count = count + 1
-                                if r < 0.01:
-                                    break
-                            print(f'The penny(s) is(are): {count}')
-
-            elif r >= 0.10:
-                count = 0
-                while True:
-                    r = r - 0.10
-                    count = count + 1
-                    if r < 0.10:
-                        break
-                print(f'The dime(s) is(are): {count}')
-
-                r = r - 0.10 * count
-                if r >= 0.05:
-                    count = 0
-                    while True:
-                        r = r - 0.05
-                        count = count + 1
-                        if r < 0.05:
-                            break
-                    print(f'The nickel(s) is(are): {count}')
-
-                    r = r - 0.05 * count
-                    if r >= 0.01:
-                        count = 0
-                        while True:
-                            r = r - 0.01
-                            count = count + 1
-                            if r < 0.01:
-                                break
-                        print(f'The penny(s) is(are): {count}')
+            count_change(r)
+    else:
+        print(f'The change dollar is :{0}')
+        r = ce
+        if r != 0:
+            count_change(r)
 
 
 
-            elif r >= 0.05:
-                count = 0
-                while True:
-                    r = r - 0.05
-                    count = count + 1
-                    if r < 0.05:
-                        break
-                print(f'The nickel(s) is(are): {count}')
-
-                r = r - 0.05 * count
-                if r >= 0.01:
-                    count = 0
-                    while True:
-                        r = r - 0.01
-                        count = count + 1
-                        if r < 0.01:
-                            break
-                    print(f'The penny(s) is(are): {count}')
-
-
-            else:
-                count = 0
-                while True:
-                    r = r - 0.01
-                    count = count + 1
-                    if r < 0.01:
-                        break
-                print(f'The penny(s) is(are): {count}')
-
-
-    return ce
 
 if __name__ == '__main__':
     cost = float(input("Please enter a cost:\n"))
     amount = float(input("Please enter the amount of money given:\n"))
-    change = change(cost, amount)
-    print(change)
+    change(cost, amount)
