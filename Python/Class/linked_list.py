33965f33e19ece5c8098beb1ab2607034939f6a3
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2020-02-17 14:10:04 -0600

    link list

--- Python/Class/linked_list.py
+++ Python/Class/linked_list.py
@@ -1,4 +1,37 @@
 class Node():
-    def __init__(self, data = "None"):
-        data = data
-        data.next = None
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+class SingleLinkedList():
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        cur = self.head
+        while cur.next!= None:
+            cur = cur.next
+        new_node = Node(data)
+        cur.next = new_node
+
+    def __str__(self):
+        elem = []
+        cur = self.head
+        while cur.next!= None:
+            cur = cur.next
+            elem.append(cur.data)
+        return str(elem)
+
+# n1 = Node(1)
+# n2 = Node(2)
+# n1.next = n2
+#
+#
+# print(n1)
+# print(n2)
+lst = SingleLinkedList()
+print(lst)
+lst.append(1)
+lst.append(2)
+lst.append(3)
+print(lst)
